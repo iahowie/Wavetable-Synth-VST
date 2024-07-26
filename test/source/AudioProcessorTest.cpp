387f3b7bb6b9c1724f66803d24cff609876bc890
Ian Howie <139757365+iahowie@users.noreply.github.com>
2024-07-26 03:22:43 -0600

    Delete test directory

--- test/source/AudioProcessorTest.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-#include <PluginName/PluginProcessor.h>
-
-namespace audio_plugin_test {
-    TEST(AudioPluginAudioProcessor, Foo) {
-        AudioPluginAudioProcessor processor{};
-        ASSERT_FALSE(true);
-    }
-}
